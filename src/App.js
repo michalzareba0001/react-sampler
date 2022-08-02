4a0bd74479c005069fc4755fd638540da1ddce49
Michał Zaręba <michalzareba0001@gmail.com>
2022-08-02 02:17:13 +0200

    starting the work - sound-pad button added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import SoundPad from './sound-pad';
 
 function App() {
   return (
-    <div className="App">
-      <div 
+      <div className="App-body">
       <header className="App-header">
         <h1>This will be a simple sampler app</h1>
       </header>
-    </div>
+      <div className="App-main">
+        <SoundPad /> <SoundPad /> <SoundPad /> <SoundPad /> <SoundPad />
+
+      </div>
+      </div>
   );
 }
 
---
+++ src/sound-pad.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import './soundpad.css';
+
+
+class SoundPad extends Component {
+    render() {
+        return (
+      
+                <button className="sound-pad">
+                    test
+                </button>
+        )
+
+    }
+
+}
+
+export default SoundPad;
